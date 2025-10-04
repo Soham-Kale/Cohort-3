ace614c8778aaa20c7520168d6dc024e11dd9867
Soham-Kale <sohamkale3014@gmail.com>
2025-10-04 23:41:03 +0530

    dom completed

---
+++ week3/index.js
@@ -0,0 +1,16 @@
+// There are 5 popular methods available for fetching DOM elements - 
+// querySelector
+// querySelectorAll
+// getElementById
+// getElementByClassName
+// getElementsByClassName
+
+//1. Fetching the title
+const title = document.querySelector('h1');
+console.log(title.innerHTML)
+
+
+const firstTodo = document.querySelector("h4");
+firstTodo.innerHTML = "Dont' take class"
+
+
